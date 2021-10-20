79e412755f10bc145d9fb7815b2d7e612bee9036
kvoththebloodless <gouravacharya19@gmail.com>
2021-10-19 23:21:55 -0600

    animation added for monster, invoke not being called bug

--- Assets/Scripts/CharacterScripts/CharacterMover.cs
+++ Assets/Scripts/CharacterScripts/CharacterMover.cs
@@ -7,7 +7,7 @@ using System;
 public class CharacterMover : MonoBehaviour
 {
     private string animationToPlay = "Walking";
-    private string animationOnceStopped = "standing";
+
     public float speed = 3;
     private NavMeshAgent agent;
 
@@ -16,35 +16,43 @@ public class CharacterMover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
+        gameObject.TryGetComponent<NavMeshAgent>(out agent);
+
+
 
     }
 
     private void Update()
     {
-        agent.speed = speed;
-        if (!agent.isStopped)
+        if (agent)
         {
-            if ((transform.position - agent.destination).magnitude < 0.2f)
+            agent.speed = speed;
+            if (!agent.isStopped)
             {
-                StopMoving();
+                if ((transform.position - agent.destination).magnitude < 0.2f)
+                {
+                    StopMoving();
+                }
             }
         }
     }
-    public Action GoToPoint(Vector3 startposition, Vector3 destination)
+    public void GoToPoint(Vector3 startposition, Vector3 destination)
     {
         transform.position = startposition;
         transform.LookAt(destination);
         agent.isStopped = false;
         animator.Play(animationToPlay);
         agent.SetDestination(destination);
-        return reachedDestination;
 
     }
+    /*    public Action GoToPointNoNavmesh(Vector3 startposition, Vector3 destination, float time)
+        {
+
+        }*/
     public void StopMoving()
     {
         reachedDestination.Invoke();
-        animator.Play(animationOnceStopped);
+
         agent.isStopped = true;
 
 
--- Assets/Scripts/Managers/Scene4Manager.cs
+++ Assets/Scripts/Managers/Scene4Manager.cs
@@ -22,7 +22,7 @@ public class Scene4Manager : MonoBehaviour
     public CharacterHeadLook baby_head_look;
     public CharacterSwitchAnimation baby_switch_animation;
 
-
+    [SerializeField] Transform baby_init_position;
     [SerializeField] Transform baby_position_1;
     [SerializeField] Transform baby_position_2;
     public Transform baby_position_3;
@@ -73,6 +73,8 @@ public class Scene4Manager : MonoBehaviour
     public CharacterMover devil_char_mover;
     public CharacterHeadLook devil_head_look;
     public CharacterSwitchAnimation devil_switch_animation;
+    public Transform demon_init_position;
+    public Transform demon_baby_position;
 
     /// <summary>
     /// DEMON SPAWNER
@@ -95,23 +97,30 @@ public class Scene4Manager : MonoBehaviour
     /// PostProcessingVolume
     ///</summary>
 
-
+    ///<summary>
+    ///lights
+    ///</summary>
+    public Light kidroomdoorlight;
     private void Awake()
     {
         instance = this;
+        kidroomdoorlight.gameObject.SetActive(false);
+        baby.transform.position = baby_init_position.position;
         GeneralEvent += (Collider collider) =>
           {
 
               if (collider.tag == "MirrorTrigger")
               {
                   Destroy(collider.gameObject);
-                  StartMirrorScene();
+                  kidroomdoorlight.gameObject.SetActive(true);
+                  StartCoroutine(executeafterntime(1, () => { StartMirrorScene(); }));
+
 
               }
               if (collider.tag == "PickUpTrigger")
               {
                   Destroy(collider.gameObject);
-                  StartBabySnatchedScene();
+                  /* StartBabySnatchedScene();*/
               }
               //TODO: WHEN FIRST CROSSED THE THRESHOLD OF THE 4TH house TRIGGER
               //--> DOOR CLOSED
@@ -127,15 +136,21 @@ public class Scene4Manager : MonoBehaviour
     public void StartMirrorScene()
     {
 
+        baby_char_mover.reachedDestination += Kidnap;
         kidsRoomDoor.OpenDoor(2, true);
-        Action a = baby_char_mover.GoToPoint(baby_position_1.position, baby_position_2.position);
-        a += () =>
-        {
 
-        };
+        baby_char_mover.GoToPoint(baby_position_1.position, baby_position_2.position);
+        baby_char_mover.reachedDestination -= Kidnap;
+
         /*      MoveCharacterToPosition.Invoke(enterRoomStartPoint.position, kidbed.position);
               ManLookAt.Invoke(Camera.main.transform);*/
 
+    }
+    public void Kidnap()
+    {
+        baby_switch_animation.switchtoanimation("sweep", 0, 1);
+
+        devil_switch_animation.switchtoanimation("kidnap", 0, 1);
     }
     public void StartBabySnatchedScene()
     {
@@ -143,10 +158,10 @@ public class Scene4Manager : MonoBehaviour
 
     }
 
-    IEnumerator executeafterntime(int n)
+    IEnumerator executeafterntime(int n, Action a)
     {
         yield return new WaitForSeconds(n);
-
+        a.Invoke();
     }
 
     public void SetDiary(bool value)
